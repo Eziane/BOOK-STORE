46c32567521feac974e09d20690fa388cfbc315a
Eziane <ezianabderrahim@gmail.com>
2023-12-11 22:58:58 +0100

    Adding the page that display the book details

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Spinner from "../../components/Spinner";
+import BackButton from '../com'
+import { useNavigate } from "react-router-dom";
+
 
 const CreateBooks = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+    .get('http://localhost:5555/books')
+    .then((response) => {
+      setBooks(response.data.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setLoading(false);
+    });
+  }, []);
   return (
-    <div>CreateBooks</div>
+    <div className="flex justify-between items-center">
+        <Link to='/'>
+          <MdOutlineAddBox className="text-sky-800 text-4x"/>
+        </Link>
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -48,11 +48,11 @@ const Home = () => {
           <tbody>
             {books.map((book, index) => (
               <tr key={book._id} className="h-8">
-                <td className=" border border-slate-700 rounded-md text-center"> {index + 1} </td>
-                <td className=" border border-slate-700 rounded-md text-center"> {book.title} </td>
-                <td className=" border border-slate-700 rounded-md text-center max-md:hidden"> {book.author} </td>
-                <td className=" border border-slate-700 rounded-md text-center max-md:hidden"> {book.publishYear} </td>
-                <td className=" border border-slate-700 rounded-md text-center">
+                <td className="border border-slate-700 rounded-md text-center"> {index + 1} </td>
+                <td className="border border-slate-700 rounded-md text-center"> {book.title} </td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden"> {book.author} </td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden"> {book.publishYear} </td>
+                <td className="border border-slate-700 rounded-md text-center">
                   <div className="flex justify-center gap-x-4">
                     <Link to={`/books/details/${book._id}`}>
                       <BsInfoCircle className="text-2xl text-green-800"/>
